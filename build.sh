d0931ba8e29ee72081072c5095093e8199b503e1
Zijie Tian <ztecube@gmail.com>
2024-10-04 21:06:32 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /bin/zsh
+
+# cmake -B build -DGGML_CUDA=ON \
+#     -DGGML_CUDA_FORCE_DMMV=ON \
+#     -DGGML_CUDA_FORCE_CUBLAS=ON \
+#     -DGGML_CUDA_F16=ON \
+#     -DGGML_AVX=ON \
+#     -DGGML_AVX2=ON \
+#     -DGGML_AVX512=ON \
+#     -DCMAKE_CUDA_ARCHITECTURES=87
+
+cmake -B build -DGGML_CUDA=ON \
+    -DGGML_CUDA_FORCE_DMMV=ON \
+    -DCMAKE_CUDA_ARCHITECTURES=87
+
+cmake --build build --config Release --target llama-cli -j 12
